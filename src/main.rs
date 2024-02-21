2b2d12f86415ab362849f4f6dbfa01b4db5838de
welliton gervickas <wellitogervickas@gmail.com>
2024-02-21 17:04:47 -0300

    fix: moving result and error to use eprintln and std

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
 
     // is it like try catch?
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
